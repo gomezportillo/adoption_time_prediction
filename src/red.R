52577ec0d62eba6631d0b8a3ddbbf2438b57449e
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-06-08 11:49:58 +0200

    Adding tensorboard, downloading dataset zip and updating neural network

--- src/red.R
+++ src/red.R
@@ -1,9 +1,16 @@
-
 #Cambio el directorio de trabajo
 setwd("C:/Users/Pedro Manuel/Desktop/SIGE")
 options(max.print = 999999)
 set.seed(1234)
 
+## -------------------------------------------------------------------------------------
+## Descargo el fichero .zip
+
+download.file(
+  "https://s3.us-east-2.amazonaws.com/pets-adoption/petfinder-adoption-prediction.zip",
+  destfile = "petfinder-adoption-prediction.zip"
+)
+zip::unzip("petfinder-adoption-prediction.zip", exdir = "petfinder-adoption-prediction")
 
 library(keras)
 
@@ -32,6 +39,8 @@ test_generator <- flow_images_from_directory(
   class_mode='categorical',
   subset='validation') # set as validation data
 
+
+
 ## -------------------------------------------------------------------------------------
 ## Crear modelo
 
@@ -41,19 +50,20 @@ model <- keras_model_sequential() %>%
   layer_conv_2d(filters = 32,  kernel_size = c(3, 3), activation = "relu", input_shape = c(150, 150, 3)) %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_conv_2d(filters = 64,  kernel_size = c(3, 3), activation = "relu") %>% layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>% layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>% layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_flatten() %>%
   layer_dense(units = 512, activation = "relu") %>%
-  layer_dense(units = 5, activation = "sigmoid")
+  layer_dense(units = 5, activation = "softmax")
 
+
+# Resumen del modelo
 summary(model)
+
 
 # Compilar modelo
 # https://tensorflow.rstudio.com/keras/reference/compile.html
 model %>% compile(
   loss = "categorical_crossentropy",
-  optimizer = optimizer_rmsprop(lr = 1e-4),
+  optimizer = optimizer_rmsprop(lr = 0.0001, decay = 1e-6),
   metrics = c("accuracy")
 )
 
@@ -64,7 +74,14 @@ history <-  model %>% fit_generator(
   train_generator,
   steps_per_epoch = 100,
   validation_steps = 50,
-  epochs = 5)
+  epochs = 15,
+  verbose = 2,
+  callbacks = list(
+    # save best model after every epoch
+    callback_model_checkpoint("./mejor_checkpoints.h5", save_best_only = TRUE),
+    # only needed for visualising with TensorBoard
+    callback_tensorboard(log_dir = "./logs")
+  ))
 
 # Evaluar modelo
 # https://tensorflow.rstudio.com/keras/reference/evaluate_generator.html
@@ -74,9 +91,14 @@ model %>% evaluate_generator(test_generator, steps = 25)
 # Visualizar entrenamiento
 plot(history)
 
+#
+tensorboard("./logs")
+
+# Imprima la información de la versión sobre R , el SO y los paquetes adjuntos o cargados.
+sessionInfo()
 
 
-
+# En proceso
 test_generator$filenames
 predictions <- predict_generator(
   model,
